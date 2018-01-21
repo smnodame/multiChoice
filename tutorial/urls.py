391d769a7c8ff8d0af20c6121989f01747286969
boonprakit <boonprakit@proteus-tech.com>
2018-01-21 19:51:00 +0700

    create api for upload file

--- tutorial/urls.py
+++ tutorial/urls.py
@@ -7,7 +7,7 @@ router = routers.DefaultRouter()
 router.register(r'users', views.UserViewSet)
 router.register(r'groups', views.GroupViewSet)
 
-# Wire up our API using automatic URL routing.
+# Wire up our API using automatAic URL routing.
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
     url(r'^', include(router.urls)),
