bcffb08ce957ca8e2373c50f625daabdc35edcce
boonprakit <boonprakit@proteus-tech.com>
2018-01-21 19:44:25 +0700

    create for upload file

--- quickstart/views.py
+++ quickstart/views.py
@@ -1,7 +1,10 @@
 from django.contrib.auth.models import User, Group
+from django.conf import settings
+
 from rest_framework import viewsets
 from quickstart.serializers import UserSerializer, GroupSerializer
-
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
 class UserViewSet(viewsets.ModelViewSet):
     """
@@ -17,3 +20,13 @@ class GroupViewSet(viewsets.ModelViewSet):
     """
     queryset = Group.objects.all()
     serializer_class = GroupSerializer
+
+
+class NewPhotoUploadView(APIView):
+    def post(self, request, format=None):
+        uploaded_file = request.FILES['file']
+        path_fmt = '{}/{survey}-{response}-{question}-{filename}'
+        with open(path_fmt.format(settings.MEDIA_ROOT, **request.data), 'wb+') as file:
+            for chunk in uploaded_file.chunks():
+                file.write(chunk)
+        return Response(status=204)
