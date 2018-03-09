d0d2e8700718d59d77478903a4583f902bc0b66d
boonprakit <boonprakit@proteus-tech.com>
2018-03-10 02:16:34 +0700

    accept the ip host

--- tutorial/settings.py
+++ tutorial/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'l(o-%vtgvw+*$aonrs06cfld+x7p7npjet&c(=p9rmopx=^xdf'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['192.168.1.39']
 
 
 # Application definition
